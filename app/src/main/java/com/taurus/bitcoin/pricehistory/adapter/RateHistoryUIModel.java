e18b88f029a15a99b960df78e2892729f093bf6e
eminuluyol <eminuluyol@gmail.com>
2017-06-30 12:49:28 +0200

    Add RateHistoryViewHolder

--- app/src/main/java/com/taurus/bitcoin/pricehistory/adapter/RateHistoryUIModel.java
+++ app/src/main/java/com/taurus/bitcoin/pricehistory/adapter/RateHistoryUIModel.java
@@ -1,6 +1,10 @@
 package com.taurus.bitcoin.pricehistory.adapter;
 
 import com.taurus.bitcoin.baseadapter.model.GenericItem;
+import com.taurus.bitcoin.network.model.pricehistory.History;
+import com.taurus.bitcoin.util.ListConverter;
+
+import java.util.List;
 
 public class RateHistoryUIModel extends GenericItem {
 
@@ -21,5 +25,19 @@ public class RateHistoryUIModel extends GenericItem {
 
     public void setAverage(String average) {
         this.average = average;
+    }
+
+    public static List<RateHistoryUIModel> createList(List<History> rateHistoryList) {
+        return ListConverter.convert(rateHistoryList, item -> create(item));
+    }
+
+    private static RateHistoryUIModel create(History item) {
+
+        final RateHistoryUIModel model = new RateHistoryUIModel();
+
+        model.setAverage(String.valueOf(item.getAverage()));
+        model.setTime(item.getTime());
+
+        return model;
     }
 }
